015a835aeecc29b416963c6f05d1e775b4b9dd91
batro <batro_ska@yahoo.es>
2019-11-02 00:38:39 +0100

    a

--- src/main/java/com/example/appTest/TestJob.java
+++ src/main/java/com/example/appTest/TestJob.java
@@ -6,7 +6,7 @@ public class TestJob implements Runnable {
 	public void run() {
 		System.out.println(Thread.currentThread().getName());
 		try {
-			Thread.sleep(3000);
+			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
